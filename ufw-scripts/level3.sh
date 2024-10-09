0ef8b646e91232be455f6356dfc09b1ad7330f1d
HeyItsSamir <SargonSamirIsaacDinkha@outlook.com>
2024-10-09 11:51:21 -0500

    Update level3.sh

--- ufw-scripts/level3.sh
+++ ufw-scripts/level3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Level 3: Allow HTTP/HTTPS, SSH, and Deny All Other Traffic
+# Level 3: Strict - Allow HTTP/HTTPS, SSH, and Deny All Other Traffic
 
 # Ask user if they want to make a backup before resetting UFW
 
